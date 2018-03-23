49c68e3635ce6a92b7afc182d24b314ee0834f4c
FiscalTiger <apouleson@gmail.com>
2018-03-22 21:10:39 -0700

    Make server that has map of handlers for easy extension
    Made a type called myHandler that contains a map of url routes to
    handler functions.  It also serves a 404 if the route has not been
    specified before.

--- helloWorld.go
+++ helloWorld.go
@@ -1,19 +1,51 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
-	"fmt"
+	"os"
 )
 
-const (
-	port = ":3000"
-)
+// Server to serve up templates and such
+type myHandler struct {
+	mux map[string]func(http.ResponseWriter, *http.Request)
+}
+
+func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mh.mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
+
+	http.NotFound(w, r)
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	io.WriteString(w, "Hello world!")
+
+}
+
+func (mh *myHandler) addRouteToMux(route string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+	mh.mux[route] = handlerFunc
 }
 
 func main() {
-	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(port, nil)
+	mh := &myHandler{mux: make(map[string]func(http.ResponseWriter, *http.Request))}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	server := http.Server{
+		Addr:    ":"+port,
+		Handler: mh,
+	}
+
+	mh.addRouteToMux("/hello", helloWorld)
+
+	log.Fatal(server.ListenAndServe())
 }
