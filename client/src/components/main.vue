83c2af2849250658563832326a3acb5ebeaead65
Swaroop Akkineni <swa7@pitt.edu>
2018-04-04 21:35:27 -0400

    vue-cli init commit

---
+++ client/src/components/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h3>{{ authors }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      title: 'Financial Freedom app',
+      authors: 'By: Swoop Dogg & White Boy'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h3 {
+  font-weight: normal;
+}
+</style>
