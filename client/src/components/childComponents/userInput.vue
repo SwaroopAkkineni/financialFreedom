b9ae006575eeda0b8032fbf041d59c7628ec8db5
Swaroop Akkineni <swa7@pitt.edu>
2018-04-04 22:02:54 -0400

    userInput form initial commit

---
+++ client/src/components/childComponents/userInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Please enter your name:{{message}}</p>
+    <input v-model="message">
+    <p>In how many years do you wan to retire in: {{yearsToRetirement}}</p>
+    <input v-model="yearsToRetirement">
+    <p>How much is your initial investment:{{initialInvestment}}</p>
+    <input v-model="initialInvestment">
+    <p>How much do you want to put in a year:{{continousInvestment}}</p>
+    <input v-model="continousInvestment">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userInput',
+  data () {
+    return {
+      message: '',
+      yearsToRetirement: '',
+      initialInvestment: '',
+      continousInvestment: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
