362731b0e563ae22e060ba73ae48989396552aab
Swaroop Akkineni <swa7@pitt.edu>
2018-04-11 18:30:38 -0400

    VueX store set up, text areaWorking

--- client/src/components/childComponents/userInput.vue
+++ client/src/components/childComponents/userInput.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <p>Please enter your name: {{message}}</p>
-    <input v-model="message" placeholder="Enter text here:">
-    <p>In how many years do you wan to retire in: {{yearsToRetirement}}</p>
-    <input v-model="yearsToRetirement" placeholder="Enter text here:">
-    <p>How much is your initial investment: {{initialInvestment}}</p>
-    <input v-model="initialInvestment" placeholder="Enter text here:">
-    <p>How much do you want to put in a year: {{continousInvestment}}</p>
-    <input v-model="continousInvestment" placeholder="Enter text here:">
-    <p>Vuex Test: {{getMessage}} </p>
+    <p>Please enter your name: -- {{getMessage}}</p>
+    <input v-model="message" v-on:change="updateMessage" placeholder="Enter text here:">
+
+    <p>In how many years do you wan to retire in:  {{getYearsToRetirement}}</p>
+    <input v-model="yearsToRetirement" v-on:change="updateYearsToRetirement" placeholder="Enter text here:">
+
+    <p>How much is your initial investment: -- {{getInitialInvestment}}</p>
+    <input v-model="initialInvestment" v-on:change="updateInitialInvestment" placeholder="Enter text here:">
+
+    <p>How much do you want to put in a year:  -- {{getContinousInvestment}}</p>
+    <input v-model="continousInvestment" v-on:change="updateContinousInvestment" placeholder="Enter text here:">
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters, mapActions} from 'vuex'
 
 export default {
   name: 'userInput',
@@ -27,8 +29,31 @@ export default {
   },
   computed: {
     ...mapGetters([
-      'getMessage'
+      'getMessage',
+      'getYearsToRetirement',
+      'getInitialInvestment',
+      'getContinousInvestment',
     ])
+  },
+  methods: {
+    ...mapActions([
+      'setMessage',
+      'setYearsToRetirement',
+      'setInitialInvestment',
+      'setContinousInvestment',
+    ]),
+    updateMessage () {
+      this.setMessage(this.message)
+    },
+    updateYearsToRetirement () {
+      this.setYearsToRetirement(this.yearsToRetirement)
+    },
+    updateInitialInvestment () {
+      this.setInitialInvestment(this.initialInvestment)
+    },
+    updateContinousInvestment () {
+      this.setContinousInvestment(this.continousInvestment)
+    },
   }
 }
 </script>
